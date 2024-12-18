e2b8a16d6d489ff5e9d3c018b1e8610c06159baf
liqiongfan <774542602@qq.com>
2024-12-18 16:53:57 +0800

    fix: 修正没有主键的时候的唯一性问题

--- cmd/leopards/cmd/mysql.go
+++ cmd/leopards/cmd/mysql.go
@@ -54,17 +54,23 @@ type {{ camel $value.TableName }} struct {
 
 func (orm *{{camel $value.TableName}}) Query() *leopards.Selector {
 {{ if ne $value.PRI "" }}	return orm.DB.Query().From({{ camel $value.TableName }}Table).Where(leopards.EQ("{{ $value.PRI }}", orm.{{camel $value.PRI}}))
-{{- else }}	return orm.DB.Query().From({{ camel $value.TableName }}Table){{- end }}
+{{- else }}	return orm.DB.Query().From({{ camel $value.TableName }}Table).WhereMap(map[string]any{ 
+{{ range $value.Columns }}        "{{ .ColumnName}}": orm.{{camel .ColumnName}},
+{{ end }}    }){{- end }}
 }
 
 func (orm *{{camel $value.TableName}}) Update() *leopards.UpdateBuilder {
 {{ if ne $value.PRI "" }}	return orm.DB.Update().Table({{ camel $value.TableName }}Table).Where(leopards.EQ("{{ $value.PRI }}", orm.{{camel $value.PRI}}))
-{{- else }}    return orm.DB.Update().Table({{ camel $value.TableName }}Table){{- end }}
+{{- else }} return orm.DB.Update().Table({{ camel $value.TableName }}Table).WhereMap(map[string]any{ 
+{{ range $value.Columns }}        "{{ .ColumnName}}": orm.{{camel .ColumnName}},
+{{ end }}    }){{- end }}
 }
 
 func (orm *{{camel $value.TableName}}) Delete() *leopards.DeleteBuilder {
 {{ if ne $value.PRI "" }}	return orm.DB.Delete().Table({{ camel $value.TableName }}Table).Where(leopards.EQ("{{ $value.PRI }}", orm.{{camel $value.PRI}}))
-{{- else }}    return orm.DB.Delete().Table({{ camel $value.TableName }}Table){{- end }}
+{{- else }} return orm.DB.Delete().Table({{ camel $value.TableName }}Table).WhereMap(map[string]any{ 
+{{ range $value.Columns }}        "{{ .ColumnName}}": orm.{{camel .ColumnName}},
+{{ end }}    }){{- end }}
 }
 
 func (orm *{{camel $value.TableName}}) Insert() *leopards.InsertBuilder {
